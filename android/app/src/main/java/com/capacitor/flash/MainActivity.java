e3b8d18ced67fd8d11a5f187975ba696539a5641
sirgatsen <sirgatsen@gmail.com>
2022-02-20 21:45:29 +0200

    Added basic app functionality to test plugin

---
+++ android/app/src/main/java/com/capacitor/flash/MainActivity.java
@@ -0,0 +1,5 @@
+package com.capacitor.flash;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { Toast } from '@capacitor/toast';
+
+import { CapacitorFlash } from 'capacitor-flash';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  lightOn: boolean;
+
+  constructor(private platform: Platform) {}
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      CapacitorFlash.isAvailable().then(async (result) => {
+        // auto switch on if available
+        if(result.value) {
+          await CapacitorFlash.switchOn({ intensity: 0.45 }).then(async () => {
+            const { value } = await CapacitorFlash.isSwitchedOn();
+            this.lightOn = value;
+          });
+        } else {
+          await Toast.show({ text: 'Flashlight not available!' });
+        }
+      })
+    });
+  }
+
+  toggleLight() {
+    CapacitorFlash.isSwitchedOn().then(async (result) => {
+      if(result.value) {
+        await CapacitorFlash.switchOff();
+      } else {
+        await CapacitorFlash.switchOn({ intensity: 1 });
+      }
+      this.lightOn = !result.value;
+    });
+  }
 
 }
